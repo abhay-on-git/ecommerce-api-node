1052504b7b6397f8adae5dacce572886976e324f
unknown <abhayagnihotri1585@gmail.com>
2024-08-15 20:45:24 +0530

    completed-routing

--- src/index.js
+++ src/index.js
@@ -14,4 +14,30 @@ app.use('/auth',authRouter)
 const userRouter = require('./routes/user.route')
 app.use('/api/users',userRouter)
 
+const productRouter = require('./routes/product.route')
+app.use('/api/products',productRouter)
+
+const adminProductRouter = require('./routes/adminProduct.route')
+app.use('/api/admin/products',adminProductRouter)
+
+const adminOrderRouter = require('./routes/adminOrder.route')
+app.use('/api/admin/orders',adminOrderRouter)
+
+const cartRouter = require('./routes/cart.route')
+app.use('/api/cart',cartRouter)
+
+const cartItemRouter = require('./routes/cartItems.route')
+app.use('/api/cart_items',cartItemRouter)
+
+const orderRouter = require('./routes/order.route')
+app.use('/api/orders',orderRouter)
+
+const reviewRouter = require('./routes/review.route')
+app.use('/api/reviews',reviewRouter)
+
+const ratingRouter = require('./routes/rating.route')
+app.use('/api/ratings',ratingRouter)
+
+
+
 module.exports = app
