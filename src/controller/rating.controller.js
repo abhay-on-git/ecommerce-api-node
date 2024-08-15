c805a55157bce43f175b6610b9c96a5f75e6b8a2
unknown <abhayagnihotri1585@gmail.com>
2024-08-15 18:42:44 +0530

    added-rating-and-review-services

--- src/controller/rating.controller.js
+++ src/controller/rating.controller.js
@@ -1,10 +1,25 @@
 const ratingService = require('../services/rating.service')
 
 const createRating = async(req,res)=>{
+    const user = req.user
     try {
         const rating = await ratingService.createRating(user,req.body)
         res.status(201).json(rating)
     } catch (error) {
         res.status(500).send({error:error.message})
     }
+}
+
+const getAllRatings = async(req,res)=>{
+    try {
+        const ratings = await ratingsService.createRating(req.params.id)
+        res.status(201).json(ratings)
+    } catch (error) {
+        res.status(500).send({error:error.message})
+    }
+}
+
+module.exports = {
+    createRating,
+    getAllRatings
 }
---
+++ src/controller/review.controller.js
@@ -0,0 +1,27 @@
+const reviewService = require('../services/review.service')
+
+const createReview = async(req,res)=>{
+    const user = req.user
+    try {
+        const review = await reviewService.createReview(user,req.body)
+        res.status(201).json(review)
+    } catch (error) {
+        res.status(500).send({error:error.message})
+    }
+}
+
+const getAllReviews = async(req,res)=>{
+    const user = req.user
+    try {
+        const reviews = await reviewService.getAllReview(req.params.id)
+        res.status(201).json(reviews)
+    } catch (error) {
+        res.status(500).send({error:error.message})
+    }
+}
+
+
+module.exports = {
+    createReview,
+    getAllReview
+}
