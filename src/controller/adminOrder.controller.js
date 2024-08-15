68b3c54c0f71369b0af8fdb7b2b5fceaa746a08c
unknown <abhayagnihotri1585@gmail.com>
2024-08-15 17:41:35 +0530

    added-admin-order-controllers

---
+++ src/controller/adminOrder.controller.js
@@ -0,0 +1,69 @@
+const orderService = require('../services/order.service')
+
+const getAllOrders = async(req,res)=>{
+    try {
+        const orders = await orderService.getAllOrders();
+        return res.status(200).json(orders);
+    } catch (error) {
+        return res.status(500).send({error:error.message})
+    }
+}
+
+const confirmOrders = async(req,res)=>{
+    const orderId = req.params.orderId
+    try {
+        const orders = await orderService.confirmOrder(orderId);
+        return res.status(200).json(orders);
+    } catch (error) {
+        return res.status(500).send({error:error.message})
+    }
+}
+
+const shipOrders = async(req,res)=>{
+    const orderId = req.params.orderId
+    try {
+        const orders = await orderService.shipOrder(orderId);
+        return res.status(200).json(orders);
+    } catch (error) {
+        return res.status(500).send({error:error.message})
+    }
+}
+
+const deliverOrders = async(req,res)=>{
+    const orderId = req.params.orderId
+    try {
+        const orders = await orderService.deliverOrder(orderId);
+        return res.status(200).json(orders);
+    } catch (error) {
+        return res.status(500).send({error:error.message})
+    }
+}
+
+const cancellOrders = async(req,res)=>{
+    const orderId = req.params.orderId
+    try {
+        const orders = await orderService.cancelOrder(orderId);
+        return res.status(200).json(orders);
+    } catch (error) {
+        return res.status(500).send({error:error.message})
+    }
+}
+
+const deleteOrders = async(req,res)=>{
+    const orderId = req.params.orderId
+    try {
+        const orders = await orderService.deleteOrder(orderId);
+        return res.status(200).json(orders);
+    } catch (error) {
+        return res.status(500).send({error:error.message})
+    }
+}
+
+module.exports = {
+    getAllOrders,
+    confirmOrders,
+    shipOrders,
+    cancellOrders,
+    deliverOrders,
+    deleteOrders
+}
